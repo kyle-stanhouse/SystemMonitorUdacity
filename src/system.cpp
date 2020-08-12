bbe052a7164b646ccabf9f55f0a6baafc111ced7
Kyle Stanhouse <kstanhouse@gmail.com>
2020-08-12 00:57:35 +0000

    added operating system

--- src/system.cpp
+++ src/system.cpp
@@ -25,16 +25,18 @@ vector<Process>& System::Processes() { return processes_; }
 std::string System::Kernel() { 
     
     //Usage: string LinuxParser::Kernel()
-    
     return LinuxParser::Kernel(); 
-    
     }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { 
+    
+    //Usage: string LinuxParser::OperatingSystem()
+    return LinuxParser::OperatingSystem(); 
+    }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
